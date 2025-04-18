7b2771aec7a7ebf543caf32f27f887c048be0c88
Andrew Theaker <andrew@theaker.name>
2025-04-18 13:36:45 +1000

    regreet working with background

--- nixosModules/features/greetd.nix
+++ nixosModules/features/greetd.nix
@@ -1,6 +1,31 @@
-#
 # greetd feature to enable a login screen using regreet
-{ pkgs, lib, ... }:
+{ pkgs, lib, config, ... }: let
+  wallpaper = pkgs.fetchurl {
+    url = "http://files.theaker.name/wallpaper/ice-planet-spaceship.jpg";
+    hash = "sha256-KJH14CWjH/Xe0xV1keQFn/lB/vgqp0ZDaK3xpzDGBg8=";
+  };
+  hyprlandConf = pkgs.writeText "hyprland.conf" ''
+    exec-once = ${lib.getExe config.programs.regreet.package}; hyprctl dispatch exit
+    debug {
+      disable_logs = false
+    }
+    misc {
+        disable_hyprland_logo = true
+        disable_splash_rendering = true
+        disable_hyprland_qtutils_check = true
+    }
+  '';
+in
 {
-  programs.regreet.enable = true;
+  programs.regreet = {
+    enable = true;
+    settings = {
+      background = {
+        path = builtins.toString wallpaper;
+        fit = "Cover";
+      };
+    };
+  };
+  
+  services.greetd.settings.default_session.command = "Hyprland --config ${builtins.toString hyprlandConf}";
 }
