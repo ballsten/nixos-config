4d578b456db1f54fc4627665038e61b7785c4375
Andrew Theaker <andrew@theaker.name>
2025-04-19 11:47:48 +1000

    added cyberdream to wezterm

--- homeManagerModules/features/wezterm.nix
+++ homeManagerModules/features/wezterm.nix
@@ -4,7 +4,7 @@
 # Install and configure wezterm just how I like it
 
 
-{ ... }:
+{ inputs, ... }:
 {
   programs.wezterm = {
     enable = true;
@@ -16,6 +16,8 @@
       config.font = wezterm.font 'FiraCode Nerd Font Mono'
       
       config.hide_tab_bar_if_only_one_tab = true
+
+      config.colors = dofile("${inputs.nixpkgs.legacyPackages.x86_64-linux.vimPlugins.cyberdream-nvim}/extras/wezterm/cyberdream.lua")
 
       return config
     '';
