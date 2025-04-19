543ef997d4c56f859d6e765265c637b0f5ea506e
Andrew Theaker <andrew@theaker.name>
2025-04-19 11:03:31 +1000

    basic look

--- homeManagerModules/features/wezterm.nix
+++ homeManagerModules/features/wezterm.nix
@@ -14,6 +14,8 @@
       local config = wezterm.config_builder()
 
       config.font = wezterm.font 'FiraCode Nerd Font Mono'
+      
+      config.hide_tab_bar_if_only_one_tab = true
 
       return config
     '';
