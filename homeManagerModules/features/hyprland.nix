12bf2c6e0e8980e280316fa06ccd645214ad8c48
Andrew Theaker <andrew@theaker.name>
2025-04-27 17:41:43 +1000

    update formatting

--- homeManagerModules/features/hyprland.nix
+++ homeManagerModules/features/hyprland.nix
@@ -1,4 +1,10 @@
-{ pkgs, lib, config, ... }: let
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+let
   wallpaper = pkgs.fetchurl {
     url = "http://files.theaker.name/wallpaper/futuristic-spaceship-pilot.jpg";
     hash = "sha256-fM8HGoicFjSpUOVM2RIOGP7QQ+BTQw0Yotp5ObxBM9Q=";
@@ -21,7 +27,6 @@ in
   ];
 
   # TODO: modularize these (terminal already done) but enable programs
-  # programs.kitty.enable = true;
   programs.firefox.enable = true;
 
   programs.waybar = {
@@ -31,18 +36,26 @@ in
         layer = "top";
         position = "bottom";
         modules-left = [ "hyprland/workspaces" ];
-        modules-right = ["battery" "clock"];
+        modules-right = [
+          "battery"
+          "clock"
+        ];
         battery = {
-            format = "{capacity}% {icon}";
-            format-icons = ["" "" "" "" ""];
+          format = "{capacity}% {icon}";
+          format-icons = [
+            ""
+            ""
+            ""
+            ""
+            ""
+          ];
         };
         clock = {
-            format-alt = "{:%a, %d. %b  %H:%M}";
+          format-alt = "{:%a, %d. %b  %H:%M}";
         };
       };
     };
-    style = 
-    ''
+    style = ''
       * {
         border: none;
         border-radius: 0;
@@ -50,7 +63,7 @@ in
         font-size: 18px;
         min-height: 0;
       }
-      
+
       window#waybar {
         background: #000000;
         color: #e5e9f0;
@@ -105,7 +118,7 @@ in
   wayland.windowManager.hyprland = {
     enable = true;
 
-    settings ={
+    settings = {
 
       ################
       ### MONITORS ###
@@ -113,7 +126,6 @@ in
 
       # See https://wiki.hyprland.org/Configuring/Monitors/
       monitor = lib.mkDefault ",preferred,auto,auto";
-
 
       ###################
       ### MY PROGRAMS ###
@@ -136,13 +148,12 @@ in
       # Autostart necessary processes (like notifications daemons, status bars, etc.)
       # Or execute your favorite apps at launch like this:
 
-      exec-once = [ 
+      exec-once = [
         "hyprpaper"
         "waybar"
       ];
       # exec-once = nm-applet &
       # exec-once = waybar & hyprpaper & firefox
-
 
       #############################
       ### ENVIRONMENT VARIABLES ###
@@ -154,7 +165,6 @@ in
         "XCURSOR_SIZE,24"
         "HYPRCURSOR_SIZE,24"
       ];
-
 
       #####################
       ### LOOK AND FEEL ###
@@ -184,61 +194,61 @@ in
 
       # https://wiki.hyprland.org/Configuring/Variables/#decoration
       decoration = {
-          rounding = 10;
+        rounding = 10;
 
-          # Change transparency of focused and unfocused windows
-          active_opacity = 1.0;
-          inactive_opacity = 1.0;
+        # Change transparency of focused and unfocused windows
+        active_opacity = 1.0;
+        inactive_opacity = 1.0;
 
-          shadow = {
-              enabled = true;
-              range = 4;
-              render_power = 3;
-              color = "rgba(1a1a1aee)";
-          };
+        shadow = {
+          enabled = true;
+          range = 4;
+          render_power = 3;
+          color = "rgba(1a1a1aee)";
+        };
 
-          # https://wiki.hyprland.org/Configuring/Variables/#blur
-          blur = {
-            enabled = true;
-            size = 3;
-            passes = 1;
+        # https://wiki.hyprland.org/Configuring/Variables/#blur
+        blur = {
+          enabled = true;
+          size = 3;
+          passes = 1;
 
-            vibrancy = 0.1696;
-          };
+          vibrancy = 0.1696;
+        };
       };
 
       # https://wiki.hyprland.org/Configuring/Variables/#animations
       animations = {
-          enabled = "yes, please :)";
+        enabled = "yes, please :)";
 
-          # Default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
+        # Default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
 
-          bezier = [ 
-            "easeOutQuint,0.23,1,0.32,1"
-            "easeInOutCubic,0.65,0.05,0.36,1"
-            "linear,0,0,1,1"
-            "almostLinear,0.5,0.5,0.75,1.0"
-            "quick,0.15,0,0.1,1"
-          ];
+        bezier = [
+          "easeOutQuint,0.23,1,0.32,1"
+          "easeInOutCubic,0.65,0.05,0.36,1"
+          "linear,0,0,1,1"
+          "almostLinear,0.5,0.5,0.75,1.0"
+          "quick,0.15,0,0.1,1"
+        ];
 
-          animation = [
-            "global, 1, 10, default"
-            "border, 1, 5.39, easeOutQuint"
-            "windows, 1, 4.79, easeOutQuint"
-            "windowsIn, 1, 4.1, easeOutQuint, popin 87%"
-            "windowsOut, 1, 1.49, linear, popin 87%"
-            "fadeIn, 1, 1.73, almostLinear"
-            "fadeOut, 1, 1.46, almostLinear"
-            "fade, 1, 3.03, quick"
-            "layers, 1, 3.81, easeOutQuint"
-            "layersIn, 1, 4, easeOutQuint, fade"
-            "layersOut, 1, 1.5, linear, fade"
-            "fadeLayersIn, 1, 1.79, almostLinear"
-            "fadeLayersOut, 1, 1.39, almostLinear"
-            "workspaces, 1, 1.94, almostLinear, fade"
-            "workspacesIn, 1, 1.21, almostLinear, fade"
-            "workspacesOut, 1, 1.94, almostLinear, fade"
-          ];
+        animation = [
+          "global, 1, 10, default"
+          "border, 1, 5.39, easeOutQuint"
+          "windows, 1, 4.79, easeOutQuint"
+          "windowsIn, 1, 4.1, easeOutQuint, popin 87%"
+          "windowsOut, 1, 1.49, linear, popin 87%"
+          "fadeIn, 1, 1.73, almostLinear"
+          "fadeOut, 1, 1.46, almostLinear"
+          "fade, 1, 3.03, quick"
+          "layers, 1, 3.81, easeOutQuint"
+          "layersIn, 1, 4, easeOutQuint, fade"
+          "layersOut, 1, 1.5, linear, fade"
+          "fadeLayersIn, 1, 1.79, almostLinear"
+          "fadeLayersOut, 1, 1.39, almostLinear"
+          "workspaces, 1, 1.94, almostLinear, fade"
+          "workspacesIn, 1, 1.21, almostLinear, fade"
+          "workspacesOut, 1, 1.94, almostLinear, fade"
+        ];
       };
 
       # Ref https://wiki.hyprland.org/Configuring/Workspace-Rules/
@@ -252,22 +262,21 @@ in
       # windowrulev2 = rounding 0, floating:0, onworkspace:f[1]
 
       # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
-      dwindle =  {
-          pseudotile = true; # Master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
-          preserve_split = true; # You probably want this
+      dwindle = {
+        pseudotile = true; # Master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
+        preserve_split = true; # You probably want this
       };
 
       # See https://wiki.hyprland.org/Configuring/Master-Layout/ for more
       master = {
-          new_status = "master";
+        new_status = "master";
       };
 
       # https://wiki.hyprland.org/Configuring/Variables/#misc
       misc = {
-          force_default_wallpaper = 0; # Set to 0 or 1 to disable the anime mascot wallpapers
-          disable_hyprland_logo = true; # If true disables the random hyprland logo / anime girl background. :(
+        force_default_wallpaper = 0; # Set to 0 or 1 to disable the anime mascot wallpapers
+        disable_hyprland_logo = true; # If true disables the random hyprland logo / anime girl background. :(
       };
-
 
       #############
       ### INPUT ###
@@ -275,31 +284,31 @@ in
 
       # https://wiki.hyprland.org/Configuring/Variables/#input
       input = {
-          kb_layout = "us";
-          kb_variant = "";
-          kb_model = "";
-          kb_options = "";
-          kb_rules = "";
+        kb_layout = "us";
+        kb_variant = "";
+        kb_model = "";
+        kb_options = "";
+        kb_rules = "";
 
-          follow_mouse = 1;
+        follow_mouse = 1;
 
-          sensitivity = 0; # -1.0 - 1.0, 0 means no modification.
+        sensitivity = 0; # -1.0 - 1.0, 0 means no modification.
 
-          touchpad = {
-              natural_scroll = true;
-          };
+        touchpad = {
+          natural_scroll = true;
+        };
       };
 
       # https://wiki.hyprland.org/Configuring/Variables/#gestures
       gestures = {
-          workspace_swipe = false;
+        workspace_swipe = false;
       };
 
       # Example per-device config
       # See https://wiki.hyprland.org/Configuring/Keywords/#per-device-input-configs for more
       device = {
-          name = "epic-mouse-v1";
-          sensitivity = -0.5;
+        name = "epic-mouse-v1";
+        sensitivity = -0.5;
       };
 
       ###################
@@ -399,12 +408,12 @@ in
 
       windowrulev2 = [
         # Ignore maximize requests from apps. You'll probably like this.
-	"suppressevent maximize, class:.*"
+        "suppressevent maximize, class:.*"
 
         # Fix some dragging issues with XWayland
         "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
       ];
     };
-  
+
   };
 }
