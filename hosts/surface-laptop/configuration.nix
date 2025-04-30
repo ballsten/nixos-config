ad43ca23b13cdd618fe052fd9e1240b209453c0d
Andrew Theaker <andrew@theaker.name>
2025-04-30 18:22:58 +1000

    add fish to surface-laptop

--- hosts/surface-laptop/configuration.nix
+++ hosts/surface-laptop/configuration.nix
@@ -39,6 +39,7 @@ in
         username = "ballsten";
         hashedPassword = "$y$j9T$pzQ45Xjuzy6kVT2wLfpK41$a6CozSBdXG.qJeFfn9TZUB0lIFCDi3XMJtxbLFXb3M8";
         hostName = "surface-laptop";
+        shell = pkgs.fish;
       };
 
       home-manager = {
