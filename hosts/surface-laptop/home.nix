c5e52feaac109e711c796822b203acefedbc39a7
ballsten <andrew@theaker.name>
2026-06-05 06:00:39 +1000

    fix: surface monitor scaling

--- hosts/surface-laptop/home.nix
+++ hosts/surface-laptop/home.nix
@@ -13,6 +13,14 @@
       # TODO: making a gaming bundle
     };
     features.hyprland.suspendEnable = true;
+    features.hyprland.monitors = /* lua */ ''
+      hl.monitor({
+        output = "eDP-1",
+        mode = "preferred",
+        position = "auto",
+        scale = "2"
+      });
+    '';
     features.discord.enable = false;
   };
 }
